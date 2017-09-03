755e91400618e57c5002552b649167f3727c04f6
Elena Z <zhizhimontova@gmail.com>
2017-09-02 20:23:10 -0700

    Fix front page [1/1]

--- src/main/java/controllers/NetIdController.java
+++ src/main/java/controllers/NetIdController.java
@@ -3,12 +3,19 @@ package controllers;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/netid")
+@Path("")
 @Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})
 public class NetIdController {
 
+    @Path("/netid")
     @GET
-    public String getReceipts() {
+    public String getNetId() {
+        return "ez256";
+    }
+
+    @Path("")
+    @GET
+    public String getNetIdMainPage() {
         return "ez256";
     }
 }
